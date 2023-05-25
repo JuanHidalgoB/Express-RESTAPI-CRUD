ee51905ed2e8170ebdb28cc567668949faf17e66
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-05-25 00:31:03 -0500

    GET y POST, GET con id

--- server.js
+++ server.js
@@ -2,15 +2,40 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+const products = [
+ {
+    id: 1,
+    product: 'laptop',
+    price: 3000
+ },
 
-app.use(morgan('dev'))
+ {
+    id: 2,
+    product:'Mouse',
+    price: 100
+ },
+
+ {
+    id: 3,
+    product:'Touchpad',
+    price: 100
+ }
+    
+]
+
+//middlewares
+app.use(morgan('dev'))//logger
+app.use(express.json())//para que pueda leer json
 
 
 app.get('/products', (req, res) => {
-    res.send('Obteniendo productos')
+    res.json(products)
 })
 
 app.post('/products', (req, res) => {
+    const newProduct = {id: products.length + 1,...req.body}
+    products.push(newProduct)
+    console.log(newProduct)
     res.send('Creando productos')
 })
 
@@ -23,7 +48,13 @@ app.delete('/products', (req, res) => {
 })
 
 app.get('/products/:id', (req, res) => {
-    res.send('Obteniendo un producto')
+    const productFound = products.find((product) => {
+        return product.id ===  parseInt(req.params.id) //lee el parametro de id como un string
+        }
+    )
+    if (!productFound) return res.status(404).send('Producto no encontrado')
+
+    res.json(productFound)
 })
 app.listen(3000)
 console.log(`server on port ${3000}`)
