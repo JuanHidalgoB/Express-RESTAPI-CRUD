597a12315efe8992f639807f71801844ca83f45b
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-05-25 01:32:57 -0500

    Modificando más métodos

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-const products = [
+let products = [
  {
     id: 1,
     product: 'laptop',
@@ -16,7 +16,7 @@ const products = [
  },
 
  {
-    id: 3,
+    id: 4,
     product:'Touchpad',
     price: 100
  }
@@ -33,23 +33,47 @@ app.get('/products', (req, res) => {
 })
 
 app.post('/products', (req, res) => {
-    const newProduct = {id: products.length + 1,...req.body}
+    let newId = 0
+
+    let idFound = products.find(p => p.id === newId) //por si ya existe el id
+
+     //newId = idFound ? newId + 1: newId
+
+    while (idFound){
+        newId++
+        idFound = products.find(p => p.id === newId)
+    }
+    
+    const newProduct = {id: newId,...req.body}
     products.push(newProduct)
     console.log(newProduct)
     res.send('Creando productos')
 })
 
-app.put('/products', (req, res) => {
+app.put('/products/:id', (req, res) => {
+    const productFound = products.find(p => p.id === parseInt(req.params.id))
+    if (!productFound) return res.status(404).send('Producto no encontrado')
+
+    const newData = req.body
+
+    products = products.map(p => p.id === parseInt(req.params.id) ? {...p, ...newData} : p)
     res.send('Actualizando productos')
 })
 
-app.delete('/products', (req, res) => {
-    res.send('Eliminando productos')
+app.delete('/products/:id', (req, res) => {
+
+    const productFound = products.find(p => p.id === parseInt(req.params.id)) //lee el parametro de id como un string por eso el parseo
+    if (!productFound) return res.status(404).send('Producto no encontrado')
+
+    products = products.filter(p => {return p.id !== parseInt(req.params.id)}) 
+
+    res.json(products)
 })
 
 app.get('/products/:id', (req, res) => {
+
     const productFound = products.find((product) => {
-        return product.id ===  parseInt(req.params.id) //lee el parametro de id como un string
+        return product.id ===  parseInt(req.params.id) //lee el parametro de id como un string por eso el parseo
         }
     )
     if (!productFound) return res.status(404).send('Producto no encontrado')
